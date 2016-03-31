3e19ed172aa7ec62d4b3202bd959af7068f7d101
Camilo Garcia <equiposoperaciones@anteksa.com>
2016-03-30 21:00:01 -0500

    Se agregan los VO de la base de datos y el DAO de las mascotas

--- src/co/edu/intecap/clinicaveterinaria/modelo/conexion/Conexion.java
+++ src/co/edu/intecap/clinicaveterinaria/modelo/conexion/Conexion.java
@@ -5,10 +5,39 @@
  */
 package co.edu.intecap.clinicaveterinaria.modelo.conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author capacitaciones
  */
 public class Conexion {
+    
+    protected static Connection cnn;
+    protected static void conectar (){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnn = DriverManager.getConnection(
+                    "jdbc:mysql:/169.254.43.21:3306/veterinaria",
+                    "root",
+                    ""
+            );
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    
+    }
+    protected static void desconectar (){
+        try {
+            cnn.close();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+        
+    }
+    
+    
+    
     
 }
